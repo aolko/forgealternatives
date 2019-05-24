e0d53b37f161982949ec090a74be198e5091cdd2
aolko <aolko93@gmail.com>
2019-05-24 10:41:32 +0300

    assets

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@ const jsonConcat = require("gulp-json-concat");
 
 
 function defaultTask(cb) {
-        console.log("Concating all json files");
+    console.log("Concating all json files");
         gulp.src('json/**/*.json')
         .pipe(jsonConcat('mods.json',function(data){
-            return new Buffer(JSON.stringify(data));
+            const array = Object.keys(data).map(filename => data[filename]);
+            return new Buffer(JSON.stringify(array));
         }))
         .pipe(gulp.dest('public'));
     cb();
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,27 @@
+var modsJson = json.stringify($.getJSON('mods.json'));
+var options = {
+    keys: ["mcmod.name", "mcmod.tags"],
+    minMatchCharLength: 3,
+    shouldSort: true
+};
+var fuse = new Fuse(modsJson, options);
 $(function() {
-
+    $('#searchbar').on('keyup', function () {
+        let result = fuse.search($(this).val());
+        console.log("val: "+$(this).val()+"; result: "+result.length);
+        // Output it
+        let resultdiv = $('ul.searchresults');
+        if (result.length === 0) {
+            // Hide results
+            resultdiv.hide();
+        } else {
+            // Show results
+            resultdiv.empty();
+            for (let item in result.slice(0,4)) {
+                let searchitem = '<li><a href="/' + result[item].path + '">' + result[item].title + '</a></li>';
+                resultdiv.append(searchitem);
+            }
+            resultdiv.show();
+        }
+    });
 });
